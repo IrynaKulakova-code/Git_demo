5796fe7f25b611f5783c73d7315ae6bf518662f6
Iryna Kulakova <kulakova.ira@gmail.com>
2023-03-31 15:01:22 -0700

    Add coding and language f

--- HW.py
+++ HW.py
@@ -5,3 +5,9 @@ class Employee:
 
     def work(self):
         return "I can work"
+
+    def coding(self):
+        return 'I can coding'
+
+    def get_language(self):
+        return f'My language is {self.language}'
